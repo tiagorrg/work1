3f979c6869f6778800ba1fe6f8ba141194fce45f
Семен <semen@MacBook-Air-Semen.local>
2024-11-28 21:12:36 +0300

    Компоненты языка JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+const info = {          //объявляем объект 
+    city: "Krasnodar",  //объявлем элемент city
+    temperature: 5      //объявлем элемент temperature
+};
+
+console.log(info.city + ` ${info.temperature} градусов`);  //выводим на консоль информации из объекта
+
+//
+
+const result = (2 + 2) * 3;     //присваем result значение примера
+console.log(result);            //выводим result в консоль
